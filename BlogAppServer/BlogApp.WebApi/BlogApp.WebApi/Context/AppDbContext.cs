37b8b7d5092e15414c15d05653003b1941036aef
caglatunc <caglatunc03@gmail.com>
2023-11-23 04:51:19 +0300

    first commit

---
+++ BlogAppServer/BlogApp.WebApi/BlogApp.WebApi/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BlogApp.WebApi.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApp.WebApi.Context;
+
+public class AppDbContext :DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    public DbSet<BlogPost> BlogPosts { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
